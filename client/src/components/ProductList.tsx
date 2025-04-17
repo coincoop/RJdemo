6e7f228968b9bfe6034e688cf0ad34d940331703
haithuy <94002982+coincoop@users.noreply.github.com>
2025-04-17 15:50:42 +0700

    add filer/sort

--- client/src/components/ProductList.tsx
+++ client/src/components/ProductList.tsx
@@ -11,15 +11,16 @@ import Loading from './Loading'
 const ProductList = () => {
     const searchParams = useSearchParams();
     const query = searchParams.get('query') || '';
+    const [sortField, setSortField] = useState<'name' | 'price'>('name');
+    const [sortOrder, setSortOrder] = useState<'asc' | 'desc'>('asc');
     const [products, setProducts] = useState([])
-    const [loading, setLoading] = useState(false)
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         fetchProducts();
     }, []);
     const fetchProducts = async () => {
         try {
-            setLoading(true);
             const res = await productsAPI.handleProduct('/get-all-product',
                 'get'
             )
@@ -29,17 +30,47 @@ const ProductList = () => {
         } catch (error) {
             console.error('Lỗi khi lấy dữ liệu sản phẩm:', error);
         } finally {
-            setLoading(false); // Tắt trạng thái loading
+            setLoading(false);
         }
     }
 
-    const filteredProducts = products.filter((product: any) =>
-        product.name.toLowerCase().includes(query.toLowerCase())
-    );
+    const sortProducts = (a: any, b: any) => {
+        let fieldA = a[sortField];
+        let fieldB = b[sortField];
+    
+        if (sortField === 'name') {
+            fieldA = fieldA.toLowerCase();
+            fieldB = fieldB.toLowerCase();
+            if (fieldA < fieldB) return sortOrder === 'asc' ? -1 : 1;
+            if (fieldA > fieldB) return sortOrder === 'asc' ? 1 : -1;
+            return 0;
+        }
+    
+        if (sortField === 'price') {
+            return sortOrder === 'asc' ? fieldA - fieldB : fieldB - fieldA;
+        }
+    
+        return 0;
+    };
+    
+    const filteredProducts = (query ? products.filter((p: any) =>
+        p.name.toLowerCase().includes(query.toLowerCase())
+    ) : [...products]).sort(sortProducts);
+    
+
 
     return (
         loading ? <Loading /> :
             <section className={style['container']}>
+                <select onChange={(e) => setSortField(e.target.value as 'name' | 'price')}>
+                    <option value="name">Sắp xếp theo tên</option>
+                    <option value="price">Sắp xếp theo giá</option>
+                </select>
+
+                <select onChange={(e) => setSortOrder(e.target.value as 'asc' | 'desc')}>
+                    <option value="asc">Tăng dần</option>
+                    <option value="desc">Giảm dần</option>
+                </select>
                 <div>
                     {
                         query ? <h1 className={style['title']}>Search results for "{query}"</h1> : <h1 className={style['title']}>All product</h1>
