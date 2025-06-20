98de8fd531819ca5e970c34ab877189e419439b3
haithuy <94002982+coincoop@users.noreply.github.com>
2025-06-20 20:40:42 +0700

    fix

--- server/middlewares/verifyToken.js
+++ server/middlewares/verifyToken.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-export const verifyToken = (req, res, next) => {
+ const verifyToken = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1]; 
 
   if (!token) return res.status(403).json({ message: 'Không có token' });
@@ -13,3 +13,5 @@ export const verifyToken = (req, res, next) => {
     res.status(401).json({ message: 'Token không hợp lệ' });
   }
 };
+
+module.exports = {verifyToken}
